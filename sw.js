d689b12f3d11973d9f10898c28fb0774c02f90e0
mauricedeschamps <mementomorikatsu@gmail.com>
2026-03-26 17:28:15 +0900

    sw.js の更新

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
 const CACHE_NAME = 'bcn-guide-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  'index.html',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
