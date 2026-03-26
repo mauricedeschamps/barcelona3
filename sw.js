3e2417f504d37efc86dbe1cc460c2ff3d62a812e
mauricedeschamps <mementomorikatsu@gmail.com>
2026-03-26 16:19:59 +0900

    sw.js の更新

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE_NAME = 'barcelona-guide-v1';
+const CACHE_NAME = 'bcn-guide-v2';
 const urlsToCache = [
   '/',
-  'index.html',
-  'icons/icon-192.png',
-  'icons/icon-512.png'
-  // 必要に応じてCSS、画像などを追加
+  '/index.html',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
